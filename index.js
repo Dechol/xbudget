df2a1585d83c83fe2eda445d6808c8328ebf0ff2
declan <declanholland91@gmail.com>
2023-08-23 00:40:05 +0700

    changed port for render.com

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import mongoose, { Schema } from 'mongoose'
 import 'dotenv/config'
 
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3001
 const idArray = []
 
 //mongoose
